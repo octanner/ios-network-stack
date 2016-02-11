94e9234b760cd0f3e66c700620051268c679e503
Ben Norris <ben@bsn.io>
2016-02-11 11:59:58 -0700

    Initial blank framework

---
+++ NetworkStack/NetworkStack.h
@@ -0,0 +1,19 @@
+//
+//  NetworkStack.h
+//  NetworkStack
+//
+//  Created by Ben Norris on 2/11/16.
+//  Copyright © 2016 OC Tanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkStack.
+FOUNDATION_EXPORT double NetworkStackVersionNumber;
+
+//! Project version string for NetworkStack.
+FOUNDATION_EXPORT const unsigned char NetworkStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkStack/PublicHeader.h>
+
+
