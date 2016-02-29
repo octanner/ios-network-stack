88e09ac586f245568d15a31542a278add60ec929
Jason Larsen <jason.larsen@gmail.com>
2016-02-29 09:02:46 -0700

    add `map` and `flatMap`
    This allows you to do something like
    
    ```
    let result: Result<JSONObject> = // JSON blah blah blah
    let userResult = result.map { User(object: $0) }
    ```

--- NetworkStack/Result.swift
+++ NetworkStack/Result.swift
@@ -11,4 +11,27 @@ import Foundation
 public enum Result<T> {
     case Ok(T)
     case Error(ErrorType)
+    
+    public func map<U>(@noescape f: T throws -> U) -> Result<U> {
+        switch self {
+        case let .Ok(x):
+            do {
+                return try .Ok(f(x))
+            }
+            catch {
+                return .Error(error)
+            }
+        case let .Error(error):
+            return .Error(error)
+        }
+    }
+    
+    public func flatMap<U>(@noescape f: T -> Result<U>) -> Result<U> {
+        switch self {
+        case let .Ok(x):
+            return try f(x)
+        case let .Error(error):
+            return .Error(error)
+        }
+    }
 }
