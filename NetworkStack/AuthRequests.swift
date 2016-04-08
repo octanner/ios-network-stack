980eb0131a1cacd9ba42d730af62842de541ec07
Tim Shadel <tim@shadelsoftware.com>
2016-04-08 15:32:44 -0600

    Refresh auth tokens

--- NetworkStack/AuthRequests.swift
+++ NetworkStack/AuthRequests.swift
@@ -13,10 +13,18 @@ public protocol AuthRequests {
     func authenticate(username: String, password: String, completion: Network.ResponseCompletion)
     func authenticate(pairingCode: String, completion: Network.ResponseCompletion)
     func authenticate(tokenJSON: JSONObject) throws
+    func refreshToken(completion: Network.ResponseCompletion)
     func logOut()
 }
 
 public struct AuthAPIRequests: AuthRequests {
+    
+    // MARK: - Types
+    
+    enum Error: ErrorType {
+        case RefreshTokenMissing
+    }
+    
 
     // MARK: - Public initializer
 
@@ -95,6 +103,45 @@ public struct AuthAPIRequests: AuthRequests {
                 do {
                     try appNetworkState.saveToken(json)
                     try appNetworkState.saveClient(json)
+                    completion(result)
+                } catch {
+                    completion(.Error(error))
+                }
+            case .Error:
+                completion(result)
+            }
+        }
+    }
+    
+    public func refreshToken(completion: Network.ResponseCompletion) {
+        guard let appNetworkState = AppNetworkState.currentAppState else { fatalError("Must configure current app state to log in") }
+        guard let refreshToken = appNetworkState.refreshToken else { return completion(.Error(Error.RefreshTokenMissing)) }
+        guard let url = NSURL(string: appNetworkState.tokenEndpointURLString) else {
+            let error = Network.Error.MalformedEndpoint(endpoint: appNetworkState.tokenEndpointURLString)
+            completion(.Error(error))
+            return
+        }
+        var parameters = [
+            "grant_type": "refresh_token",
+            "refresh_token" : refreshToken
+        ]
+        if let client = appNetworkState.client {
+            parameters["client_id"] = client.id
+            parameters["client_secret"] = client.secret
+        }
+        
+        NSURLCache.sharedURLCache().removeAllCachedResponses()
+        
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.HTTPAdditionalHeaders = ["content-type": "application/json"]
+        configuration.timeoutIntervalForRequest = 10.0
+        let session = NSURLSession(configuration: configuration)
+        
+        network.post(url, session: session, parameters: parameters) { result in
+            switch result {
+            case let .Ok(json):
+                do {
+                    try appNetworkState.saveToken(json)
                     completion(result)
                 } catch {
                     completion(.Error(error))
